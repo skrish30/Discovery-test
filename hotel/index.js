4105fe8e33ac25d9c294c4757962e33b35880305
Subhakrish.K <k.subahkrish@gmail.com>
2019-06-06 12:30:16 +0100

    fix bugs

--- hotel/index.js
+++ hotel/index.js
@@ -10,7 +10,6 @@ const io = require('socket.io')(server);
 const fsPromises=require('fs').promises;
 const fs = require('fs');
 require('dotenv').config({silent: true});
-require("./data.json")
 
 //modules for V2 assistant
 var bodyParser = require('body-parser'); // parser for post requests
@@ -44,8 +43,8 @@ const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
     url: process.env.NATURAL_LANGUAGE_UNDERSTANDING_URL
   });
 
-const environmentID = process.env.ENVIRONMENT_ID;
-const collectionID = process.env.COLLECTION_ID;
+const environmentID = process.env.DISCOVERY_ENVIRONMENT_ID;
+const collectionID = process.env.DISCOVERY_COLLECTION_ID;
 
 // start server on the specified port and binding host
 server.listen(appEnv.port, '0.0.0.0', function() {
@@ -57,6 +56,15 @@ server.listen(appEnv.port, '0.0.0.0', function() {
 app.get('/', function(req, res){
   res.sendFile('index.html');
 });
+
+//creat necessary documents
+fsPromises.writeFile("categories.json",'')
+        .then(()=> console.log("categories created"))
+        .catch(()=> console.log("failure"))
+
+fsPromises.writeFile("data.json",'')
+        .then(()=> console.log("data created"))
+        .catch(()=> console.log("failure"))
 
 
 /*****************************
@@ -87,21 +95,19 @@ const queryParams = {
   filter: "id::\"39f38b9c1cd046d3ac50d7f8ace63fe0\""
 };
 
-// discovery.query(queryParams)
-//   .then(queryResponse => {
-//     console.log(JSON.stringify(queryResponse, null, 2));
-//   data = JSON.stringify(queryResponse, null, 2);
-//     fsPromises.writeFile("data.txt", data)
-// 	.then(()=> console.log("success"))
-// 	.catch(()=> console.log("failure"))
-//   })
-//   .catch(err => {
-//     console.log('error:', err);
-//   });
+discovery.query(queryParams)
+  .then(queryResponse => {
+    //print query results
+    //console.log(JSON.stringify(queryResponse, null, 2));
+  data = JSON.stringify(queryResponse, null, 2);
+    fsPromises.writeFile("data.json", data)
+	.then(()=> console.log("success"))
+	.catch(()=> console.log("failure"))
+  })
+  .catch(err => {
+    console.log('error:', err);
+  });
 
-fsPromises.writeFile("categories.json",'')
-          .then(()=> console.log("file created"))
-          .catch(()=> console.log("failure"))
 
 
 //reads file and returns the JSON object
@@ -127,10 +133,12 @@ return new Promise(function(resolve, reject) {
 })
 };
 
+app.get('/interest', (req,res)=>{
+
 readJson('data.json')
-.then((res)=> {
+.then((results)=> {
     elements = [];
-    res.forEach(concepts=>{
+    results.forEach(concepts=>{
     const analyzeParams = {
         'text':'I am interested in ' + concepts,
         'features': {
@@ -139,7 +147,8 @@ readJson('data.json')
           }
         }
       };
-      console.log(analyzeParams.text);
+      //print all the concepts
+      //console.log(analyzeParams.text);
       
       naturalLanguageUnderstanding.analyze(analyzeParams)
         .then(analysisResults => {
@@ -159,55 +168,23 @@ readJson('data.json')
     setTimeout(function() {
             //console.log(elements[0].categories[0].label)
             console.log('matched interests')
-            userInterest = 'science';
+            userInterest = 'hobbies and interests';
             elements.forEach((concept)=>{
                 let interest = concept.categories[0].label;
-                if(interest === userInterest){
-                    console.log( console.log(concept.concepts))
-                };
-                //console.log(concept.categories[0].label.slice(1) + "\/")
                 interest = concept.categories[0].label.slice(1) + "\/";
                 num = interest.search('\/')
                 interest =interest.slice(0,num);
-                console.log(interest)
+                if(interest === userInterest){
+                    console.log(concept.concepts);
+                };
+                //print out the avaliable interests
+                //console.log(concept.categories[0].label, "\t\t" + concept.concepts)
             });
+            return res.status(200).json({mes:'Successs'})
         }, 5000);
 })
 .catch((err)=> console.log("error:",err))
-
-
-
-
-
-// readJson('data.json', (queryResults,callback)=>{
-//     elements = [];
-//     queryResults.forEach(element=>{
-//     const analyzeParams = {
-//         'text':'I am interested in ' + element,
-//         'features': {
-//           'categories': {
-//             'limit': 3
-//           }
-//         }
-//       };
-
-//       console.log(analyzeParams.text);
-      
-//       naturalLanguageUnderstanding.analyze(analyzeParams)
-//         .then(analysisResults => {
-//             elements.push(analysisResults)
-//             //let data = analyzeParams.text + "\n" + JSON.stringify(analysisResults, null, 2);
-//         })
-//         .catch(err => {
-//           console.log('error:', err);
-//         });
-//     });
-// });
-
-
-
-
-
+});
 
 
 
